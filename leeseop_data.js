4fb0650b488d10096474088cda18fabfb811a8a2
최재영 <choijihoon9988@gmail.com>
2025-11-25 17:27:39 +0900

    Gemini

--- leeseop_data.js
+++ leeseop_data.js
@@ -13,134 +13,134 @@ window.COURSE_DATA_LEESEOP = {
         {
             title: "Chapter 0. 오리엔테이션 & 마인드셋",
             lectures: [
-                { title: "1. 연애 조차 마음대로 못 하면서 잘난 척 하는 사람들이 제일 우스운 이유", duration: "11:38", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 나의 인생 이야기", duration: "35:23", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 이 강의를 수강하며 가져야 하는 마인드세팅", duration: "06:08", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 연애 조차 마음대로 못 하면서 잘난 척 하는 사람들이 제일 우스운 이유", duration: "11:38", videoId: "https://youtu.be/r-zjlm48wd4" },
+                { title: "2. 나의 인생 이야기", duration: "35:23", videoId: "https://youtu.be/Vf5jK3hNLiE" },
+                { title: "3. 이 강의를 수강하며 가져야 하는 마인드세팅", duration: "06:08", videoId: "https://youtu.be/SoW85DrPpMw" }
             ]
         },
         {
             title: "Chapter 1. 남녀의 본성과 심리에 대한 근본적인 이해와 개념 정리",
             lectures: [
-                { title: "1. 인간은 왜 살아가는가?", duration: "13:48", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 인생이 생각대로 풀리지 않는 이유", duration: "13:31", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 상위법 우선의 원칙", duration: "13:03", videoId: "VIDEO_ID_HERE" },
-                { title: "4. 남녀간의 문제가 생기는 이유", duration: "13:48", videoId: "VIDEO_ID_HERE" },
-                { title: "5. PUA이론, 레드필 이론 등.. 남성 연애학의 역사와 한계", duration: "46:56", videoId: "VIDEO_ID_HERE" },
-                { title: "6. 연애 초보들이 가장 많이 하는 착각과 충격적인 진실 1", duration: "38:43", videoId: "VIDEO_ID_HERE" },
-                { title: "7. 연애 초보들이 가장 많이 하는 착각과 충격적인 진실 2", duration: "26:00", videoId: "VIDEO_ID_HERE" },
-                { title: "8. 여성의 마음이 움직이는 4단계 메커니즘", duration: "35:27", videoId: "VIDEO_ID_HERE" },
-                { title: "9. 여자들은 항상 거짓말을 한다 (현대남녀의 성전략)", duration: "17:02", videoId: "VIDEO_ID_HERE" },
-                { title: "10. 내가 운명론자들을 싫어하지 않는 이유", duration: "30:49", videoId: "VIDEO_ID_HERE" },
-                { title: "11. 여성이 가장 우선시하는 가치 - 가장 빠르게 상위 3%의 남성이 되는 방법", duration: "22:38", videoId: "VIDEO_ID_HERE" },
-                { title: "12. 남성과 여성이 S파트너로만 남게 되는 원리", duration: "22:17", videoId: "VIDEO_ID_HERE" },
-                { title: "13. 밀당이 중요하다는 말의 진정한 의미", duration: "15:20", videoId: "VIDEO_ID_HERE" },
-                { title: "14. 픽업 아티스트, 연애 유튜버들의 현실적인 한계", duration: "31:12", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 인간은 왜 살아가는가?", duration: "13:48", videoId: "https://youtu.be/SQFHLHv5bYQ" },
+                { title: "2. 인생이 생각대로 풀리지 않는 이유", duration: "13:31", videoId: "https://youtu.be/Ex-HazlEyuc" },
+                { title: "3. 상위법 우선의 원칙", duration: "13:03", videoId: "https://youtu.be/D0s4Q0Z5JLY" },
+                { title: "4. 남녀간의 문제가 생기는 이유", duration: "13:48", videoId: "https://youtu.be/UvpCFAW3SCI" },
+                { title: "5. PUA이론, 레드필 이론 등.. 남성 연애학의 역사와 한계", duration: "46:56", videoId: "https://youtu.be/05JUXvm5B00" },
+                { title: "6. 연애 초보들이 가장 많이 하는 착각과 충격적인 진실 1", duration: "38:43", videoId: "https://youtu.be/qyA1kztJ5os" },
+                { title: "7. 연애 초보들이 가장 많이 하는 착각과 충격적인 진실 2", duration: "26:00", videoId: "https://youtu.be/qyA1kztJ5os" },
+                { title: "8. 여성의 마음이 움직이는 4단계 메커니즘", duration: "35:27", videoId: "https://youtu.be/e682H_xX8nM" },
+                { title: "9. 여자들은 항상 거짓말을 한다 (현대남녀의 성전략)", duration: "17:02", videoId: "https://youtu.be/3ydfZ5jN_rg" },
+                { title: "10. 내가 운명론자들을 싫어하지 않는 이유", duration: "30:49", videoId: "https://youtu.be/v50zpj6YZEk" },
+                { title: "11. 여성이 가장 우선시하는 가치 - 가장 빠르게 상위 3%의 남성이 되는 방법", duration: "22:38", videoId: "https://youtu.be/oXBQ4QHxwB0" },
+                { title: "12. 남성과 여성이 S파트너로만 남게 되는 원리", duration: "22:17", videoId: "https://youtu.be/30jBHzZuH8M" },
+                { title: "13. 밀당이 중요하다는 말의 진정한 의미", duration: "15:20", videoId: "https://youtu.be/BH4KYjs6duo" },
+                { title: "14. 픽업 아티스트, 연애 유튜버들의 현실적인 한계", duration: "31:12", videoId: "https://youtu.be/gRA3z6WCo6k" }
             ]
         },
         {
             title: "Chapter 2. 이성을 만나기 전 필수로 갖춰야 할 것들",
             lectures: [
-                { title: "1. 너는 지금 연애가 문제가 아니다", duration: "30:28", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 나랑 똑같은 쟤는 무시를 안 당하는데, 왜 나만 무시를 당하는 걸까?", duration: "19:46", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 방구석에서도 사회성을 획기적으로 높이는 방법들", duration: "13:03", videoId: "VIDEO_ID_HERE" },
-                { title: "4. 실행력과 지속력을 획기적으로 높이는 방법들", duration: "27:15", videoId: "VIDEO_ID_HERE" },
-                { title: "5. 인생에서 반드시 멀리해야 하는 것들 10가지", duration: "44:59", videoId: "VIDEO_ID_HERE" },
-                { title: "6. 남자라면 평생 꾸준히 해야하는 것들 10가지", duration: "32:24", videoId: "VIDEO_ID_HERE" },
-                { title: "7. 학교에서는 가르쳐 주지 않는 것들", duration: "14:58", videoId: "VIDEO_ID_HERE" },
-                { title: "8. 수많은 자수성가 부자들의 인생을 바꾼 <20% 에너지 이론>", duration: "15:40", videoId: "VIDEO_ID_HERE" },
-                { title: "9. 일과 사랑을 동시에 잡는 방법", duration: "11:48", videoId: "VIDEO_ID_HERE" },
-                { title: "10. 최단기 속성으로 훈남 되는 스타일링 비법 (실제 스타일리스트 초대)", duration: "28:48", videoId: "VIDEO_ID_HERE" },
-                { title: "11. 80%이상의 연애 초보는 신경쓰지 않지만 굉장히 중요한 부분", duration: "09:06", videoId: "VIDEO_ID_HERE" },
-                { title: "12. 준비는 끝났다. 최후의 마인드세팅..!", duration: "07:59", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 너는 지금 연애가 문제가 아니다", duration: "30:28", videoId: "https://youtu.be/yHxPdXQXb3s" },
+                { title: "2. 나랑 똑같은 쟤는 무시를 안 당하는데, 왜 나만 무시를 당하는 걸까?", duration: "19:46", videoId: "https://youtu.be/EHHE-0yUFSg" },
+                { title: "3. 방구석에서도 사회성을 획기적으로 높이는 방법들", duration: "13:03", videoId: "https://youtu.be/qF4dtY82obo" },
+                { title: "4. 실행력과 지속력을 획기적으로 높이는 방법들", duration: "27:15", videoId: "https://youtu.be/Bf8YIGyiAHg" },
+                { title: "5. 인생에서 반드시 멀리해야 하는 것들 10가지", duration: "44:59", videoId: "https://youtu.be/6tOYKYBBzPw" },
+                { title: "6. 남자라면 평생 꾸준히 해야하는 것들 10가지", duration: "32:24", videoId: "https://youtu.be/-ngkqRxMnxY" },
+                { title: "7. 학교에서는 가르쳐 주지 않는 것들", duration: "14:58", videoId: "https://youtu.be/-uTNBVTv-g4" },
+                { title: "8. 수많은 자수성가 부자들의 인생을 바꾼 <20% 에너지 이론>", duration: "15:40", videoId: "https://youtu.be/fphZLQ3QKcM" },
+                { title: "9. 일과 사랑을 동시에 잡는 방법", duration: "11:48", videoId: "https://youtu.be/OcN6uxM2cyY" },
+                { title: "10. 최단기 속성으로 훈남 되는 스타일링 비법 (실제 스타일리스트 초대)", duration: "28:48", videoId: "https://youtu.be/FSPyTvV3MmY" },
+                { title: "11. 80%이상의 연애 초보는 신경쓰지 않지만 굉장히 중요한 부분", duration: "09:06", videoId: "https://youtu.be/9b4xqTzEcFQ" },
+                { title: "12. 준비는 끝났다. 최후의 마인드세팅..!", duration: "07:59", videoId: "https://youtu.be/O-bBLlcEmFY" }
             ]
         },
         {
             title: "Chapter 3. 수학의 정석 보다 중요한 의사소통의 정석",
             lectures: [
-                { title: "1. <재미>있는 남자를 원한다는 여자들의 진짜 속마음 - 롤러코스터 이론", duration: "20:10", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 뻔한 주제는 안 된다고? 여자와는 대체 '무엇'을 이야기해야 할까?", duration: "18:43", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 남성으로 보이는 비언어적 요소", duration: "12:36", videoId: "VIDEO_ID_HERE" },
-                { title: "4. 백트래킹, 스토리텔링 등이 실전에서 정말 효과가 있을까?", duration: "16:17", videoId: "VIDEO_ID_HERE" },
-                { title: "5. 여자의 감정을 읽는 모든 방법", duration: "19:49", videoId: "VIDEO_ID_HERE" }
+                { title: "1. <재미>있는 남자를 원한다는 여자들의 진짜 속마음 - 롤러코스터 이론", duration: "20:10", videoId: "https://youtu.be/vAss-GborU8" },
+                { title: "2. 뻔한 주제는 안 된다고? 여자와는 대체 '무엇'을 이야기해야 할까?", duration: "18:43", videoId: "https://youtu.be/Wfx9KSSNct8" },
+                { title: "3. 남성으로 보이는 비언어적 요소", duration: "12:36", videoId: "https://youtu.be/j-iW0EISp-U" },
+                { title: "4. 백트래킹, 스토리텔링 등이 실전에서 정말 효과가 있을까?", duration: "16:17", videoId: "https://youtu.be/FHOFtp8D5xU" },
+                { title: "5. 여자의 감정을 읽는 모든 방법", duration: "19:49", videoId: "https://youtu.be/OPik6ksjXjU" }
             ]
         },
         {
             title: "Chapter 4. 오프라인에서 인연을 만드는 모든 방법",
             lectures: [
-                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "11:51", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 길거리, 카페, 매장, 대중교통 등", duration: "16:17", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 학교, 회사, 알바, 기타 모든 모임", duration: "09:53", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "11:51", videoId: "https://youtu.be/RIPJMDaGp6Y" },
+                { title: "2. 길거리, 카페, 매장, 대중교통 등", duration: "16:17", videoId: "https://youtu.be/70CRSpgKUec" },
+                { title: "3. 학교, 회사, 알바, 기타 모든 모임", duration: "09:53", videoId: "https://youtu.be/cIs4lr1kvS8" }
             ]
         },
         {
             title: "Chapter 5. 온라인에서 인연을 만드는 모든 방법 (소개팅 어플, SNS등)",
             lectures: [
-                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "07:03", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 성공률을 획기적으로 높이는 구체적인 방법", duration: "06:29", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "07:03", videoId: "https://youtu.be/mO9rBfgbIu8" },
+                { title: "2. 성공률을 획기적으로 높이는 구체적인 방법", duration: "06:29", videoId: "https://youtu.be/pTHmAhRL_7E" }
             ]
         },
         {
             title: "Chapter 6. 당일에 즉시 합석하는 방법 (술집, 라운지바, 번화가 등)",
             lectures: [
-                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "11:34", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 장소/상황별 구체적 공략법, 모든 경우의 수 대처법", duration: "13:51", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "11:34", videoId: "https://youtu.be/b92fH2E_qac" },
+                { title: "2. 장소/상황별 구체적 공략법, 모든 경우의 수 대처법", duration: "13:51", videoId: "https://youtu.be/gX2pErPpytQ" }
             ]
         },
         {
             title: "Chapter 7. 여자는 응급실에 갈 때도 폰을 놓지 않는다, 연락의 기술",
             lectures: [
-                { title: "1. 마인드 세팅", duration: "13:37", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 번호를 알게 된 직후", duration: "02:40", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 번호를 원래 알고있는 경우", duration: "03:32", videoId: "VIDEO_ID_HERE" },
-                { title: "4. 기본적인 대화의 틀", duration: "05:51", videoId: "VIDEO_ID_HERE" },
-                { title: "5. 절대 하면 안되는 것들", duration: "05:14", videoId: "VIDEO_ID_HERE" },
-                { title: "6. 타입별 대처법 (읽씹, 안읽씹, 단답, 느린답장, 의심 등)", duration: "04:20", videoId: "VIDEO_ID_HERE" },
-                { title: "7. 데이트 신청 타이밍", duration: "03:29", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 마인드 세팅", duration: "13:37", videoId: "https://youtu.be/a7KcQ_WSizQ" },
+                { title: "2. 번호를 알게 된 직후", duration: "02:40", videoId: "https://youtu.be/wf0mOPjhi70" },
+                { title: "3. 번호를 원래 알고있는 경우", duration: "03:32", videoId: "https://youtu.be/Er5z3USFKys" },
+                { title: "4. 기본적인 대화의 틀", duration: "05:51", videoId: "https://youtu.be/47ayI6b4ACk" },
+                { title: "5. 절대 하면 안되는 것들", duration: "05:14", videoId: "https://youtu.be/frmFU19PL1k" },
+                { title: "6. 타입별 대처법 (읽씹, 안읽씹, 단답, 느린답장, 의심 등)", duration: "04:20", videoId: "https://youtu.be/Jwn14tu_Rt8" },
+                { title: "7. 데이트 신청 타이밍", duration: "03:29", videoId: "https://youtu.be/YOLvxleBq0E" }
             ]
         },
         {
             title: "Chapter 8. 1:1 첫만남",
             lectures: [
-                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "10:17", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 처음부터 끝까지 이대로만 하면 됩니다", duration: "20:11", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "10:17", videoId: "https://youtu.be/-QPxIXfo6vE" },
+                { title: "2. 처음부터 끝까지 이대로만 하면 됩니다", duration: "20:11", videoId: "https://youtu.be/IFVSQpOadlE" }
             ]
         },
         {
             title: "Chapter 9. 두번째 만남 및 그 이후",
             lectures: [
-                { title: "1. 이대로만 하면 됩니다", duration: "03:37", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 고백의 방법 및 상황에 따른 대처법", duration: "03:37", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 자연스럽고 올바른 스킨십의 방법 - 감.명 이론", duration: "06:38", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 이대로만 하면 됩니다", duration: "03:37", videoId: "https://youtu.be/XpBbPwOUaWI" },
+                { title: "2. 고백의 방법 및 상황에 따른 대처법", duration: "03:37", videoId: "https://youtu.be/PleH0FAU05Y" },
+                { title: "3. 자연스럽고 올바른 스킨십의 방법 - 감.명 이론", duration: "06:38", videoId: "https://youtu.be/OY1wrj5U9iw" }
             ]
         },
         {
             title: "Chapter 10. 드디어 연애의 시작",
             lectures: [
-                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "07:54", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 더치페이? 6:4? 데이트 비용의 현실적인 해결방안", duration: "04:51", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 여자친구가 이별을 말하는 이유 / 권태기 극복법", duration: "06:35", videoId: "VIDEO_ID_HERE" },
-                { title: "4. 결혼은 어떤 여자와 하는 게 맞을까?", duration: "04:56", videoId: "VIDEO_ID_HERE" },
-                { title: "5. 오로지 남성만을 위한 현실적이고 이기적인 연애 전략", duration: "06:40", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 기본 개념 정리, 마인드 세팅", duration: "07:54", videoId: "https://youtu.be/c5OhP7uBkf4" },
+                { title: "2. 더치페이? 6:4? 데이트 비용의 현실적인 해결방안", duration: "04:51", videoId: "https://youtu.be/mz0KoCnogsE" },
+                { title: "3. 여자친구가 이별을 말하는 이유 / 권태기 극복법", duration: "06:35", videoId: "https://youtu.be/JeZ4zNNUVJQ" },
+                { title: "4. 결혼은 어떤 여자와 하는 게 맞을까?", duration: "04:56", videoId: "https://youtu.be/bcLPUTvHvYk" },
+                { title: "5. 오로지 남성만을 위한 현실적이고 이기적인 연애 전략", duration: "06:40", videoId: "https://youtu.be/GQ9n58gk0NY" }
             ]
         },
         {
             title: "Chapter 11. 초보 / 중수 / 고수 등급별 속성 전략",
             lectures: [
-                { title: "1. 등급별 속성 전략 (강의 전체를 봐야 이해 가능)", duration: "08:50", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 등급별 속성 전략 (강의 전체를 봐야 이해 가능)", duration: "08:50", videoId: "https://youtu.be/8zpgjTPAoA8" }
             ]
         },
         {
             title: "Chapter 12. 대한민국 최상위 0.1%인 여성을 만나는 방법",
             lectures: [
-                { title: "1. 대한민국 최상위 0.1%인 여성을 만나는 방법 - 기본 개념 정리, 마인드 세팅", duration: "12:35", videoId: "VIDEO_ID_HERE" },
-                { title: "2. 유명인을 만났을 때는 상대를 모른 척 해야 한다? 그게 바로 못 만나본 티를 내는 것", duration: "03:52", videoId: "VIDEO_ID_HERE" },
-                { title: "3. 실제 유명인들의 여러 사례를 바탕으로 여태까지의 이론 검증", duration: "10:24", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 대한민국 최상위 0.1%인 여성을 만나는 방법 - 기본 개념 정리, 마인드 세팅", duration: "12:35", videoId: "https://youtu.be/uza9yUYP0U4" },
+                { title: "2. 유명인을 만났을 때는 상대를 모른 척 해야 한다? 그게 바로 못 만나본 티를 내는 것", duration: "03:52", videoId: "https://youtu.be/J45TDgeHm3M" },
+                { title: "3. 실제 유명인들의 여러 사례를 바탕으로 여태까지의 이론 검증", duration: "10:24", videoId: "https://youtu.be/ix7OibTSyo4" }
             ]
         },
         {
             title: "Chapter 13. 마치며...",
             lectures: [
-                { title: "1. 후회와 실패 투성이였던 내 인생을 후회 없는 인생으로 바꾼 마법", duration: "09:49", videoId: "VIDEO_ID_HERE" }
+                { title: "1. 후회와 실패 투성이였던 내 인생을 후회 없는 인생으로 바꾼 마법", duration: "09:49", videoId: "https://youtu.be/8USZ-v8hJXA" }
             ]
         }
     ]
